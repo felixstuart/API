a1cedfcc57eb1920d613aa2bd4a84474fa1de949
felixstuart <148880497+felixstuart@users.noreply.github.com>
2024-11-19 17:27:54 -0500

    nice pokedex layout

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,23 +1,109 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.text.ParseException;
+import java.util.*;
+
 
 public class Pokemon {
-    public static String name;
-    public static String[] abilities;
-    public static String imageURL;
-    public final String pokeURL;
+    public String name;
+    public List<String> abilities;
+    public String imageURL;
+    public String pokeURL;
+    public ImageIcon sprite;
 
     public Pokemon(JSONObject jsonObject) {
         pokeURL = null;
         name = jsonObject.get("name").toString();
         JSONArray abilityObjects = (JSONArray) jsonObject.get("abilities");
-        abilities = (JSONArray) ((JSONArray) jsonObject.get("abilities")).stream().map(o -> {
-            ((JSONObject) ((JSONObject) o).get("ability")).get("name"))});
 
+        abilities = abilityObjects.stream().map(abilityObject -> {
+            JSONObject ability = (JSONObject) abilityObject;
+            return (String) ((JSONObject) ((JSONObject) abilityObject).get("ability")).get("name");
+        }).toList();
+
+        imageURL = (String) ((JSONObject) jsonObject.get("sprites")).get("front_default");
+//        partially adapted from https://chatgpt.com/c/673bd6dd-8eb4-8001-a9c3-7f704b44d03c
+        sprite = null;
+        try {
+            URL url = URI.create("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png").toURL();
+            BufferedImage bufferedImage = ImageIO.read(url);
+            sprite = new ImageIcon(bufferedImage.getScaledInstance(300,300, BufferedImage.SCALE_SMOOTH));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public Pokemon(String pokeURL) {
-        this.pokeURL = pokeURL;
+    public Pokemon(URL pokeURL) throws ParseException, org.json.simple.parser.ParseException {
+        this.pokeURL = String.valueOf(pokeURL);
+        JSONObject jsonObject = pull();
 
+        name = jsonObject.get("name").toString();
+        JSONArray abilityObjects = (JSONArray) jsonObject.get("abilities");
+
+        abilities = abilityObjects.stream().map(abilityObject -> {
+            JSONObject ability = (JSONObject) abilityObject;
+            return ((JSONObject) ((JSONObject) abilityObject).get("ability")).get("name");
+        }).toList();
+
+        imageURL = (String) ((JSONObject) jsonObject.get("sprites")).get("front_default");
+    }
+
+    public JSONObject pull() throws ParseException, org.json.simple.parser.ParseException {
+        String output = "abc";
+        String totaljson = "";
+        try {
+
+            URL url = new URL(pokeURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            while ((output = br.readLine()) != null) {
+                totaljson += output;
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JSONParser parser = new JSONParser();
+        org.json.simple.JSONObject jsonObject = (org.json.simple.JSONObject) parser.parse(totaljson);
+
+        try {
+            return jsonObject;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String makePokeURL(String pokemonName) {
+        return ("https://pokeapi.co/api/v2/pokemon/" + pokemonName);
     }
 }
--- src/ReadJson.java
+++ src/ReadJson.java
@@ -4,22 +4,15 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.awt.*;
-import java.util.Objects;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 
 // video to load jar
 //https://www.youtube.com/watch?v=QAJ09o3Xl_0
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -29,7 +22,7 @@ public class ReadJson {
     final private int HEIGHT = 800;
     final private int WIDTH = 700;
 
-    public static void main(String args[]) throws ParseException {
+    public static void main(String args[]) throws ParseException, java.text.ParseException {
         // In java JSONObject is used to create JSON object
         // which is a subclass of java.util.HashMap.
 
@@ -44,28 +37,63 @@ public class ReadJson {
 
     }
 
-    public ReadJson() {
-        makeUI();
+    public ReadJson() throws java.text.ParseException, ParseException {
+        Pokemon pokemon = null;
         try {
-            pull();
+            pokemon = pull("ditto");
         } catch (Exception e) {
-            System.out.println(e);
+
         }
+        makeUI(pokemon);
     }
 
     private void makeUI(Pokemon pokemon) {
         JFrame pokedex = new JFrame();
         pokedex.setSize(WIDTH, HEIGHT);
-        pokedex.setLayout(new GridLayout(2,1));
+        pokedex.setLayout(new GridBagLayout());
 
-        JLabel nameTitle = new JLabel("Name");
-        JLabel nameText = new JLabel("Ditto");
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
 
-        pokedex.add(newTitleAndContentVertical("Name", "Ditto"));
+        JLabel sprite = new JLabel(pokemon.sprite);
+        sprite.setSize(1000, 1000);
+        c.gridx = 0;
+        c.gridy = 1;
+        c.weightx = 0.5;
+        pokedex.add(sprite, c);
 
-        JLabel ablilitiesTitle = new JLabel("Abilities");
-        ablilitiesTitle.setFont(new Font("Tahoma", Font.BOLD, 14));
-        pokedex.add(ablilitiesTitle);
+        JPanel name = new JPanel();
+
+        JLabel nameTitleText = new JLabel("Name");
+        nameTitleText.setFont(new Font("Tahoma", Font.BOLD, 14));
+        JLabel contentText = new JLabel(capFirstLetter(pokemon.name));
+
+        name.setLayout(new GridLayout(2, 1));
+        name.add(nameTitleText);
+        name.add(contentText);
+        c.gridx = 1;
+        c.gridy = 1;
+        c.weightx = 0.5;
+        pokedex.add(name, c);
+
+        JPanel abilities = new JPanel();
+        abilities.setLayout(new GridLayout(3, 1));
+        JLabel abilitiesTitleText = new JLabel("Abilities");
+        abilitiesTitleText.setFont(new Font("Tahoma", Font.BOLD, 14));
+        abilities.add(abilitiesTitleText);
+
+        for (String abilityText : pokemon.abilities) {
+            JLabel abilityTextJLabel = new JLabel(capFirstLetter(abilityText));
+            abilities.add(abilityTextJLabel);
+        }
+        c.gridx = 2;
+        c.gridy = 1;
+        c.weightx = 0.5;
+        pokedex.add(abilities, c);
+
+
+
+
         pokedex.setVisible(true);
 
     }
@@ -77,19 +105,19 @@ public class ReadJson {
         titleText.setFont(new Font("Tahoma", Font.BOLD, 14));
         JLabel contentText = new JLabel(content);
 
-        panel.setLayout(new GridLayout(2,1));
+        panel.setLayout(new GridLayout(2, 1));
         panel.add(titleText);
         panel.add(contentText);
 
         return panel;
     }
 
-    public void pull() throws ParseException {
+    public Pokemon pull(String name) throws ParseException {
         String output = "abc";
         String totaljson = "";
         try {
 
-            URL url = new URL("https://pokeapi.co/api/v2/pokemon/ditto");
+            URL url = new URL("https://pokeapi.co/api/v2/pokemon/" + name);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
@@ -125,22 +153,17 @@ public class ReadJson {
         System.out.println(jsonObject);
 
         try {
-
-
-            System.out.println(jsonObject);
-            System.out.println(jsonObject.get("name").toString());
-            JSONArray abilities = (JSONArray) jsonObject.get("abilities");
-            System.out.print("Abilities:");
-            for (Object ability : abilities) {
-                System.out.print(", " + ((JSONObject) ((JSONObject) ability).get("ability")).get("name"));
-            }
-            System.out.println(jsonObject.get("order"));
+            return new Pokemon(jsonObject);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
+        return null;
     }
+
+    private String capFirstLetter(String s) {
+        return s.substring(0, 1).toUpperCase() + s.substring(1);
+    }
+
 }
 
 
